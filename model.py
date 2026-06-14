005ef4074f1baca3e54f1e51f00f42db028a3dae
Amisha103 <bhatiaamisha74@gmail.com>
2026-06-14 22:35:33 +0530

    EDA for price

--- model.py
+++ model.py
@@ -2,6 +2,21 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
 
+#ANALYSING THE DATA
 df = pd.read_csv(r"ford.csv")
 print(df.shape)
+print(df.info())
+print(df.describe()) #gets mean and everything of numeric columns
+print(df.isnull().sum())
+
+
+#EDA FOR PRICE
+# sns.histplot(df["price"], bins = 50, kde = True)# kde = true means it shows the line like how my data is distributed
+# plt.show()
+
+print(df.corr(numeric_only=True))
+sns.heatmap(df.corr(numeric_only=True),  annot = True)
+plt.show()
